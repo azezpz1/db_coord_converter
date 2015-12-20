8c5537162430688e1a4bbda48889c93e69ba64c4
Anthony Panettiere <azezpz1@gmail.com>
2015-12-20 11:45:10 -0600

    Writes database

--- coord_converter.py
+++ coord_converter.py
@@ -1,18 +1,27 @@
 import argparse, os, sqlite3, utm
 
 class db_converter:
+    ROW_NUM = 16090
+
     def __init__(self, db_addr):
         db_dir = os.path.dirname(db_addr)
-        conv_path = os.path.join(db_dir, 'coverted.db')
+        conv_path = os.path.join(db_dir, 'converted.db')
+        if os.path.isfile(conv_path):
+            os.remove(conv_path)
         conn = sqlite3.connect(db_addr)
         self.orig_c = conn.cursor()
-        self.columns = [description[0] for description in self.orig_c.description]
+        self.columns = ["markernum", "title", "indexname", "address", "city", 
+                        "county", "countyid", "utm_zone", "utm_east", 
+                        "utm_north", "code", "year", "rthl", "loc_desc", "size", 
+                        "markertext", "atlas_number"]
         self.UTM_ZONE_ELEMENT = self.columns.index("utm_zone")
         self.UTM_EAST_ELEMENT = self.columns.index("utm_east")
         self.UTM_NORTH_ELEMENT = self.columns.index("utm_north")
 
-        conn = sqlite3.connect(conv_path)
-        self.conv_c = conn.cursor()
+        self.orig_c.execute('''SELECT * FROM tx_markets''')
+
+        self.conn = sqlite3.connect(conv_path)
+        self.conv_c = self.conn.cursor()
 
         # Create table
         self.conv_c.execute('''CREATE TABLE tx_markers (markernum TEXT, 
@@ -24,9 +33,22 @@ class db_converter:
 
     def next(self):
         row = self.orig_c.fetchone()
+        if not row:
+            return
+
+        row = list(row)
         zone = row[self.UTM_ZONE_ELEMENT]
         east = row[self.UTM_EAST_ELEMENT]
         north = row[self.UTM_NORTH_ELEMENT]
+        if not zone or not east or not north:
+            self.write_blank(row)
+            return
+        zone = int(zone)
+        east = int(east)
+        north = int(north)
+        if north<0 or north > 10000000:
+            self.write_blank(row)
+            return
         row.pop(self.UTM_ZONE_ELEMENT) # removes unneeded element
         lat, lon = utm.to_latlon(east, north, zone, northern=True)
         row[self.UTM_EAST_ELEMENT] = lat
@@ -35,17 +57,19 @@ class db_converter:
         self.conv_c.execute('''INSERT INTO tx_markers VALUES (?,?,?,?,?,?,?,?,
                             ?,?,?,?,?,?,?,?)''', row)
 
-    def db_length(self):
-        # Returns the number of rows in the database
-        rows = self.orig_c.execute('''SELECT * FROM tx_markers''')
-        return len(rows)
-
+    def write_blank(self, row):
+        row.pop(self.UTM_ZONE_ELEMENT) # removes unneeded element
+        self.conv_c.execute('''INSERT INTO tx_markers VALUES (?,?,?,?,?,?,?,?,
+                            ?,?,?,?,?,?,?,?)''', row)
 
 def main():
     parser = argparse.ArgumentParser(description='Converts a database to lat/long')
-    parser.add_argument("address", type=String)
+    parser.add_argument("address")
     args = parser.parse_args()
     db_conv = db_converter(args.address)
+    for i in range(db_conv.ROW_NUM):
+        db_conv.next()
+    db_conv.conn.commit()
 
 if __name__ == '__main__':
     main()
